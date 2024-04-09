ed37af4d41a1b65b52b0dfe108976c709bebfa4e
Arun K <zen.code.symphony@gmail.com>
2024-04-09 18:58:12 +0530

    feat: added react couter and react query.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <h1>Adopt me!</h1>
-      </div>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <header className="mx-auto my-0 flex w-[1100px] content-center justify-center px-0 pb-0 pt-5">
+          <Link
+            to="/"
+            className="mx-0 my-5 block h-[76px] w-[279px] overflow-hidden bg-[url(http://static.frontendmasters.com/resources/2019-05-02-complete-intro-react-v5/image-logo.png)] pb-2.5 indent-[-9999px] font-[bold] text-[3px] text-[#333] no-underline brightness-[150%]"
+          >
+            Adopt Me!
+          </Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,45 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+
+import fetchPet from "./fetchPet";
+
 const Details = () => {
-  return <h2>hi!</h2>;
+  const { id } = useParams();
+  const results = useQuery({
+    queryKey: ["details", id],
+    queryFn: fetchPet,
+  });
+
+  if (results.isError) {
+    return <h2>Oh no! {results.error.message}</h2>;
+  }
+
+  if (results.isLoading) {
+    return (
+      <div className="flex items-center justify-center p-[15px]">
+        <h2 className="animate-spin text-[50px]">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="mx-auto my-0 mb-[25px] w-[1100px] rounded-md bg-lightpink p-[15px] shadow-[0px_0px_12px_#aaa,-0px_-0px_12px_#fff]">
+      <div>
+        <h1 className="mx-0 my-[5px] text-center text-6xl text-[#333]">
+          {pet.name}
+        </h1>
+        <h2 className="mx-0 mb-5 mt-[5px] text-center">
+          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
+          <button className="mx-auto my-0 block cursor-pointer rounded-[5px] border-[#333] border-[solid] bg-primary px-[25px] py-[5px] text-lg text-[white]">
+            Adopt {pet.name}
+          </button>
+          <p className="px-[15px] pt-5 leading-normal">{pet.description}</p>
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Details;

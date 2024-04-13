5f9d3dde2c13896388cdba6123a2179df7d463fb
Arun K <zen.code.symphony@gmail.com>
2024-04-14 01:19:13 +0530

    feat: added SSR.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Suspense, lazy, useState } from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 
 import AdoptedPetContext from "./AdoptedPetContext";
 import Loader from "./Loader";
@@ -21,26 +21,21 @@ const App = () => {
   const adoptedPet = useState(null);
   return (
     <div className="m-0 bg-[url(https://pets-images.dev-apis.com/pets/wallpaperA.jpg)] bg-repeat p-0">
-      <BrowserRouter>
-        <AdoptedPetContext.Provider value={adoptedPet}>
-          <QueryClientProvider client={queryClient}>
-            <Suspense fallback={<Loader />}>
-              <header className="mb-10 w-full bg-gradient-to-b from-yellow-500 via-orange-500 to-red-500 p-7 text-center">
-                <Link
-                  to="/"
-                  className="text-6xl text-white hover:text-gray-200 "
-                >
-                  Adopt Me!
-                </Link>
-              </header>
-              <Routes>
-                <Route path="/details/:id" element={<Details />} />
-                <Route path="/" element={<SearchParams />} />
-              </Routes>
-            </Suspense>
-          </QueryClientProvider>
-        </AdoptedPetContext.Provider>
-      </BrowserRouter>
+      <AdoptedPetContext.Provider value={adoptedPet}>
+        <QueryClientProvider client={queryClient}>
+          <Suspense fallback={<Loader />}>
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-500 via-orange-500 to-red-500 p-7 text-center">
+              <Link to="/" className="text-6xl text-white hover:text-gray-200 ">
+                Adopt Me!
+              </Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </Suspense>
+        </QueryClientProvider>
+      </AdoptedPetContext.Provider>
     </div>
   );
 };

19f545f4f52cd584b003decca31c6aeaa19702c5
Arun K <zen.code.symphony@gmail.com>
2024-04-13 22:59:55 +0530

    feat: updated ui styling.

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -6,15 +6,15 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
     hero = images[0];
   }
   return (
-    <Link to={`/details/${id}`} className="relative block">
+    <Link to={`/details/${id}`} className="relative block rounded-md">
       <img
         src={hero}
         alt={name}
         className="w-full object-cover object-center"
       />
-      <div className="absolute bottom-0 left-0 bg-gradient-to-tr from-white to-transparent pr-2 pt-2">
-        <h1>{name}</h1>
-        <h2>
+      <div className="absolute bottom-0 left-0 w-full bg-gradient-to-tr from-white to-transparent pb-2 pr-2 pt-2">
+        <h1 className="ml-2 font-semibold">{name}</h1>
+        <h2 className="ml-2 font-light">
           {animal} - {breed} - {location}
         </h2>
       </div>
